4c525c8a65ad14a750a8288f57484103d8716906
swaroophumane <36727484+swaroophumane@users.noreply.github.com>
2018-04-10 02:01:54 +0530

    Add files via upload

--- EnglishDictionary.py
+++ EnglishDictionary.py
@@ -1,29 +1,28 @@
 import json
 from difflib import get_close_matches
 
-
-data = json.load(open('data.json'))
-
-def translate(w):
-	if w in data:
-		return data[w]
-	elif len(get_close_matches(w, data.keys())) > 0:
-		yn = input(f"Did you mean '{get_close_matches(w, data.keys())[0]}' instead? Enter Y if yes, N if no. ")
-		if yn == 'Y':
-			return data[get_close_matches(w, data.keys())[0]]
-		elif yn =='N':
-			return "The Word Dosent Exist, Please Double Check It..!"
+with open('data.json'):
+	data = json.load(open('data.json'))
+	def translate(w):
+		if w in data:
+			return data[w]
+		elif len(get_close_matches(w, data.keys())) > 0:
+			yn = input(f"Did you mean '{get_close_matches(w, data.keys())[0]}' instead? Enter Y if yes, N if no. ")
+			if yn == 'Y':
+				return data[get_close_matches(w, data.keys())[0]]
+			elif yn =='N':
+				return "The Word Dosent Exist, Please Double Check It..!"
+			else:
+				return "We Didnt Understand Your Query"
 		else:
-			return "We Didnt Understand Your Query"
+			return 'The Word Dosent Exist, Please Double Check It..!'
+
+	word = input("Enter Word: ").lower()
+
+	output = translate(word)
+
+	if type(output) == list:
+		for item in output:
+			print("- " + item)
 	else:
-		return 'The Word Dosent Exist, Please Double Check It..!'
-
-word = input("Enter Word: ").lower()
-
-output = translate(word)
-
-if type(output) == list:
-	for item in output:
-		print("- " + item)
-else:
-	print(output)
+		print(output)
